7361f0b4cd4db41a04516094eb879b3f707d5115
Dickson Karis <dicksonkariuki4@gmail.com>
2019-08-26 00:34:46 +0300

    Add a hover mouse over effect on portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,82 @@ $(document).ready(function() {
 $("form.data").submit(function(event) {
   var name = $("input#name").val();
   var email = $("input#email").val();
-  var textarea = $("input textarea").val();
+  var textarea = $("input#message").val();
   if (
     $("input#name").val() &&
     $("input#email").val() &&
-    $("input textarea").val() != ""
+    $("input#message").val() != ""
   ) {
     alert(name + ".We have received your message.Thankyou for reaching us.");
   } else {
     alert("please input all your details");
   }
   event.preventDefault();
+});
+$(document).ready(function() {
+  $("#work4")
+    .mouseenter(function() {
+      $(".cover4").show();
+    })
+    .mouseleave(function() {
+      $(".cover4").hide();
+    });
+
+  $("#work3")
+    .mouseenter(function() {
+      $(".cover3").show();
+    })
+    .mouseleave(function() {
+      $(".cover3").hide();
+    });
+
+  $("#work2")
+    .mouseenter(function() {
+      $(".cover2").show();
+    })
+    .mouseleave(function() {
+      $(".cover2").hide();
+    });
+
+  $("#work1")
+    .mouseenter(function() {
+      $(".cover1").show();
+    })
+    .mouseleave(function() {
+      $(".cover1").hide();
+    });
+});
+
+$(document).ready(function() {
+  $("#work5")
+    .mouseenter(function() {
+      $(".cover5").show();
+    })
+    .mouseleave(function() {
+      $(".cover5").hide();
+    });
+
+  $("#work6")
+    .mouseenter(function() {
+      $(".cover6").show();
+    })
+    .mouseleave(function() {
+      $(".cover6").hide();
+    });
+
+  $("#work7")
+    .mouseenter(function() {
+      $(".cover7").show();
+    })
+    .mouseleave(function() {
+      $(".cover7").hide();
+    });
+
+  $("#work8")
+    .mouseenter(function() {
+      $(".cover8").show();
+    })
+    .mouseleave(function() {
+      $(".cover8").hide();
+    });
 });
