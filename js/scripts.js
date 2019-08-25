14fb1a65667f54f936f1b766d8867fc87abd4805
Dickson Karis <dicksonkariuki4@gmail.com>
2019-08-25 16:46:30 +0300

    Add project files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".design").click(function() {
+    $(".designe").toggle("slow");
+  });
+  $(".development").click(function() {
+    $(".developments").toggle("slow");
+  });
+  $(".management").click(function() {
+    $(".productsm").toggle("slow");
+  });
+  $(".what").css("text-align", "center");
+});
