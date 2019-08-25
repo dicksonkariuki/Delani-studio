cc45cee9abf9b10dda930025083a71eb52361265
Dickson Karis <dicksonkariuki4@gmail.com>
2019-08-25 19:49:59 +0300

    Add form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-  $(".design").click(function() {
-    $(".designe").toggle("slow");
+  $("#design").click(function() {
+    $(".dezine").toggle(1000);
+    $(".img1").toggle(1000);
   });
-  $(".development").click(function() {
-    $(".developments").toggle("slow");
+  $("#development").click(function() {
+    $(".developments").toggle(1000);
+    $(".img2").toggle(1000);
   });
   $(".management").click(function() {
-    $(".productsm").toggle("slow");
+    $(".productsm").toggle(100);
   });
   $(".what").css("text-align", "center");
+});
+$("form.data").submit(function(event) {
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var textarea = $("input textarea").val();
+  if (
+    $("input#name").val() &&
+    $("input#email").val() &&
+    $("input textarea").val() != ""
+  ) {
+    alert(name + ".We have received your message.Thankyou for reaching us.");
+  } else {
+    alert("please input all your details");
+  }
+  event.preventDefault();
 });
